7b9f993f0a39356d1e36e0d749b32fbf6f011f8a
towa0131 <towa.towa0131@gmail.com>
2024-05-10 17:15:02 +0900

    [Update] Update home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in
-      @posts = Post.all.order(created_at: "DESC")
       @new_post = current_user.posts.new
+      following_ids = "SELECT followed_id FROM follow_relationships WHERE follower_id = :user_id"
+      @posts = Post.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: current_user.id).order(created_at: "DESC")
     end
   end
 
